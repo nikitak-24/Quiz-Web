af83e313c0bda5cc6637bef617b1e8e8e8ae7a0f
nikitak-24 <nikitakadambala@mgmail.com>
2022-01-21 20:12:03 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "input tag is -",
+    answer: "an empty tag",
+    options: [
+      "a format tag",
+      "an empty tag",
+      "All of the above",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: " HTML is a subset of - ",
+    answer: "SGML",
+    options: [
+      "SGMD",
+      "SGMH",
+      "SGML",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The hr tag in HTML is used for?",
+    answer: " horizontal ruler",
+    options: [
+      "new line",
+      "new paragraph",
+      " horizontal ruler",
+      "vertical ruler"
+    ]
+  },
+
+
+];
